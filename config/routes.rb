935dc4b089032c270ae330515e4815ad228259c7
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-11-05 04:21:15 +0000

    init

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'todos/index'
+  get 'todos/show'
+  get 'todos/new'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
